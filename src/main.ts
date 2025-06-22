b10450e58ab85cc9999c4e2978df830dcfa486aa
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-06-22 23:11:57 +0530

    update cross policy

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthService } from './security/auth.service';
 import { AuthModule } from './security/auth.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3500);
 
   const authService = app.select(AuthModule).get(AuthService);
   await authService.seedUser();
+  logger.log('Predefined user seeded');
 
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Application is running on port ${port}`);
 }
 bootstrap();
